d45d61e675c1b6379ae39ce8e7d37ab2c05bbf39
Akshay-Kibbcom <akshay.kashyap@kibbcom.in>
2019-10-18 16:37:39 +0530

    Test1

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var app = express()
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
     res.send('hello world')
   })
   
-  app.listen(3000 , function(){
-    console.log("Listening to port 3000")
+  app.listen(port , function(){
+    console.log("Listening to port ", port)
   })
